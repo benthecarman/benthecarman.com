315c7a15a7ead8916771471e4dd4d03fe3a39a8b
Ben Carman <benthecarman@live.com>
2019-08-24 15:36:38 -0500

    Updates

---
+++ precache-manifest.3ed3cc48286de71cd1ea47f39e17acec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "851e32974aa1bcd29549",
+    "url": "/benthecarman.com/static/js/runtime~main.78358b08.js"
+  },
+  {
+    "revision": "8cee0d96536ade04a44f",
+    "url": "/benthecarman.com/static/js/main.c2e14573.chunk.js"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/benthecarman.com/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "8cee0d96536ade04a44f",
+    "url": "/benthecarman.com/static/css/main.33953eb4.chunk.css"
+  },
+  {
+    "revision": "4a7c62f7036c079db85ff01be9455a51",
+    "url": "/benthecarman.com/index.html"
+  }
+];
