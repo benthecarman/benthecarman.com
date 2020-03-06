a667b2da9e7991dbc1ce0bfe16eb0f0f44af10f7
Ben Carman <benthecarman@live.com>
2020-03-06 06:49:35 -0600

    Updates

---
+++ precache-manifest.0bc6172040c3236ad78ea3391baff09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ca178c55459edb6544140629532baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0df920dd3dd5d42f8da",
+    "url": "/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "b7c7d05ae85b9b95d8b4",
+    "url": "/static/js/2.c3a16611.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c3a16611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0df920dd3dd5d42f8da",
+    "url": "/static/js/main.482fe40e.chunk.js"
+  },
+  {
+    "revision": "7b7355f18ca4e5b8cb26",
+    "url": "/static/js/runtime-main.3bb03058.js"
+  }
+]);
