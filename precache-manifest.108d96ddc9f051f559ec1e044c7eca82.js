ad0a1268e7ca8663a7ee823182006900c22cadac
Ben Carman <benthecarman@live.com>
2020-05-31 16:50:23 -0500

    Updates

---
+++ precache-manifest.108d96ddc9f051f559ec1e044c7eca82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ed85d8b48afd005f1e0",
+    "url": "/static/js/main.ce9193fc.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.521e897d.chunk.js"
+  },
+  {
+    "revision": "8ed85d8b48afd005f1e0",
+    "url": "/static/css/main.3bd7b8d4.chunk.css"
+  },
+  {
+    "revision": "225bb7631d3454334602df3dd9b3ba9b",
+    "url": "/index.html"
+  }
+];
