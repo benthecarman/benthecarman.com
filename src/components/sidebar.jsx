438db10099e2e3b2264eb817bb661c290e72da82
benthecarman <benthecarman@live.com>
2022-12-19 23:02:09 -0600

    Change picture

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -11,7 +11,7 @@ export default class Sidebar extends Component
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center" style={{ verticalalign: 'middle' }}>
-              <div className="author-img" style={{ backgroundImage: 'url(images/me.jpg)' }} />
+              <div className="author-img" style={{ backgroundImage: 'url(images/me-bear.png)' }} />
               <h1 id="colorlib-logo"><a href="index.html">benthecarman</a></h1>
               <span className="email"><i className="icon-mail"></i><a style={{ color: '#424242', textDecoration: 'underline' }} href="mailto:benthecarman@live.com">benthecarman@live.com</a></span>
             </div>
